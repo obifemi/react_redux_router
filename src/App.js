b1331ca1ac58dd7ce1063473793d4d94ddab8db7
ali mert inal <mert.inal@gmail.com>
2023-07-08 21:59:25 +0200

    add profile

--- src/App.js
+++ src/App.js
@@ -1,42 +1,27 @@
-import Counter from './components/Counter'
-import CounterActions from './components/CounterActions'
-import { connect } from "react-redux"
-import Header from './components/Header'
-import Footer from './components/Footer'
-import {routes} from './routes'
-import {
-	BrowserRouter as Router,
-	Switch,
-	Route,
-	Redirect,
-} from 'react-router-dom';
+// App.js
 
-const mapStateToProps = state => ({
-	dark: state.site.dark,
-	user: state.auth.user
-})
-function App({ dark, user }) {
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import Profile from './views/Profile';
 
-	return (
-		<Router>
-			<div className={dark ? 'dark' : 'light'}>
-				<Header/>
-				{/*<Counter />*/}
-				{/*<CounterActions />*/}
-				<Switch>
-					{routes.map(route => (
-						<Route exact={route.exact} path={route.path} render={() => {
-							if (route.auth && !user) {
-								return <Redirect to="/login"/>
-							}
-							return <route.component/>
-						}}/>
-					))}
-				</Switch>
-				<Footer/>
-			</div>
-		</Router>
-	);
+function App() {
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/profile">Profilee</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Switch>
+          <Route path="/profile" component={Profile} />
+        </Switch>
+      </div>
+    </Router>
+  );
 }
 
-export default connect(mapStateToProps)(App);
+export default App;
